38564b0d695e54ca087fbe76ac0bc4e6ab87f0f9
Kevin Benton <blak111@gmail.com>
2013-07-12 14:08:54 -0700

    Point to master branch of devstack

--- openstack/install-devstack.sh
+++ openstack/install-devstack.sh
@@ -18,7 +18,7 @@ RESTPROXY_CONF_DIR='/etc/neutron/plugins/bigswitch'
 STACK_PASSWORD=${2:-'nova'}
 STACK_TOP='/opt/stack'
 DEVSTACK_REPO='http://github.com/bigswitch/devstack.git'
-DEVSTACK_BRANCH='bigswitch'
+DEVSTACK_BRANCH='master'
 DEVSTACK_DIR='devstack'
 
 #
