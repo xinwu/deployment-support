000e357aba889f2dab36aefeba0edeaf6136a1ab
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2013-09-11 10:51:27 -0700

    BVS-708
    Fix script to work with Red Hat as well. Two things had to be fixed:
    1. The conf files are in a different location for Red Hat
    2. Some of the default configuration (notably the message queue handler to a
    noop) had to be overriden.
    
    The script also now detects the distro (Red Hat versus Ubuntu) and then
    picks up the configuration accordingly.

--- openstack/sync_network.py
+++ openstack/sync_network.py
@@ -24,74 +24,82 @@
 #   ./sync_network.py
 #
 
-
+import platform
+import re
 import sys
 import warnings
-import logging
-import getopt
+warnings.filterwarnings("ignore")
 
 from oslo.config import cfg
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    from quantum.plugins.bigswitch.plugin import QuantumRestProxyV2
+from quantum.openstack.common import log as logging
+from quantum.plugins.bigswitch.plugin import QuantumRestProxyV2
 
+
+RED_HAT = 'red hat'
+UBUNTU  = 'ubuntu'
+DISTRO = None
 
 def get_config_files():
     """Get config file for restproxy"""
-    return [
-        "/etc/quantum/quantum.conf",
-        "/etc/quantum/dhcp_agent.ini",
-        "/etc/quantum/plugins/bigswitch/restproxy.ini",
-    ]
+    if DISTRO in [RED_HAT]:
+        return [
+            "/usr/share/quantum/quantum-dist.conf",
+            "/etc/quantum/quantum.conf",
+            "/etc/quantum/dhcp_agent.ini",
+            "/etc/quantum/plugin.ini",
+        ]
+    elif DISTRO in [UBUNTU]:
+        return [
+            "/etc/quantum/quantum.conf",
+            "/etc/quantum/dhcp_agent.ini",
+            "/etc/quantum/plugins/bigswitch/restproxy.ini",
+        ]
 
 
 def init_config():
     """Initialize configuration for this script"""
-    logging.basicConfig()
+    logging.setup("sync_network")
     cfgfile = get_config_files()
     cfg.CONF(
         args = [j for i in zip(["--config-file"]*len(cfgfile), cfgfile)
                   for j in i],
         project = "quantum",
     )
+    cfg.CONF.set_override('control_exchange', '')
+    cfg.CONF.set_override('rpc_backend', 'quantum.openstack.common.rpc.impl_fake')
+    cfg.CONF.set_override('verbose', True)
+    cfg.CONF.set_override('debug', True)
 
 
-def send_all_data(timeout):
+def send_all_data():
     """Send all data to the configured network controller
-       retunrs: None on success, else the reason for error (string)
+       returns: None on success, else the reason for error (string)
     """
     try:
-        rproxy = QuantumRestProxyV2(server_timeout=timeout)
+        rproxy = QuantumRestProxyV2()
         print "INFO: Using servers: ", cfg.CONF.RESTPROXY.servers
         rproxy._send_all_data()
     except Exception as e:
         return e.message
     return None
 
-def main(argv):
-    init_config()
-    timeout = None
-    try:
-        opts, args = getopt.getopt(argv,"ht:",["timeout="])
-    except getopt.GetoptError:
-        print 'sync_network.py -t <timeout>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'sync_network.py -t <timeout>'
-            sys.exit()
-        elif opt in ("-t", "--timeout"):
-            timeout = int(arg)
 
-    ret = send_all_data(timeout)
+if __name__ == "__main__":
+    linux_distro = platform.linux_distribution()[0]
+    print "INFO: Detected linux distro: ", linux_distro
+    if re.search(RED_HAT, linux_distro, re.IGNORECASE):
+        DISTRO = RED_HAT
+    elif re.search(UBUNTU, linux_distro, re.IGNORECASE):
+        DISTRO = UBUNTU
+    else:
+        print "ERROR: Linux distro not supported"
+        sys.exit(1)
+    init_config()
+    ret = send_all_data()
     if ret is not None:
         print "ERROR: In sending data to network controller"
         print "       " + str(ret)
         sys.exit(1)
     print "INFO: Sync Done. All data (re)sent to the network controller"
     sys.exit(0)
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
-
