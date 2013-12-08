a4f23d748649699b38c1f38fdf1364d72abcb609
Hao Li <hao.li@bigswitch.com>
2013-12-07 22:56:44 -0800

    Added clean up codes. Added security group settings to allow ssh/ping.

--- openstack-hao/test-smoke.sh
+++ openstack-hao/test-smoke.sh
@@ -6,10 +6,49 @@ set -x
 N=coke
 CIDR=10.203.10.0/24
 
+# ----------------------------------------------------------------------------
+# Clean up
+# ----------------------------------------------------------------------------
+
+unset -e
+
+export OS_USERNAME=$N
+export OS_PASSWORD=$N
+export OS_TENANT_NAME=$N
+
+nova delete $N-vm2
+nova delete $N-vm1
+
+nova secgroup-delete-rule default icmp -1 -1 0.0.0.0/0
+nova secgroup-delete-rule default tcp 22 22 0.0.0.0/0
+
+nova keypair-delete $N-key
+
+neutron router-interface-add $N-router $N-subnet
+neutron router-create $N-router
+neutron subnet-create --name $N-subnet $N-network $CIDR
+neutron net-create $N-network
+
+export OS_USERNAME=admin
+export OS_PASSWORD=ADMIN_PASS
+export OS_TENANT_NAME=admin
+
+keystone user-delete $N
+keystone tenant-delete $N
+
+# ----------------------------------------------------------------------------
+# Test
+# ----------------------------------------------------------------------------
+
+set -e
+
+export OS_USERNAME=admin
+export OS_PASSWORD=ADMIN_PASS
+export OS_TENANT_NAME=admin
+
 keystone tenant-create --name $N
 keystone user-create --name $N --tenant $N --pass $N --email $N@bigswitch.com
 
-# All operations below will be performed as user $N tenant $N.
 export OS_USERNAME=$N
 export OS_PASSWORD=$N
 export OS_TENANT_NAME=$N
@@ -19,11 +58,12 @@ neutron subnet-create --name $N-subnet $N-network $CIDR
 neutron router-create $N-router
 neutron router-interface-add $N-router $N-subnet
 
-if [ ! -f ~/.ssh/id_rsa ]; then
-    ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
-fi
-
+test -f ~/.ssh/id_rsa || ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
 nova keypair-add --pub_key ~/.ssh/id_rsa.pub $N-key
+
+nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
+nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
+nova secgroup-list-rules default
 
 nova boot --flavor m1.small --image Ubuntu-12.04 --key_name $N-key $N-vm1
 nova boot --flavor m1.small --image Ubuntu-12.04 --key_name $N-key $N-vm2
