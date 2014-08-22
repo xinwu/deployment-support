32a5c68fd496c941ef24656c4d32f5eee2013282
Kevin Benton <blak111@gmail.com>
2014-08-22 10:17:51 -0700

    Disable sync for bug

--- openstack/fuel/big_patch.py
+++ openstack/fuel/big_patch.py
@@ -1001,6 +1001,26 @@ ini_setting { "bigswitch_auth":
   require => File[$conf_dirs],
 }
 
+# temporarily disable sync until fixed
+ini_setting { "bigswitch_auto_sync":
+  path    => $neutron_conf_path,
+  section => 'restproxy',
+  setting => 'auto_sync_on_failure',
+  value   => 'False',
+  ensure  => present,
+  notify => Exec['restartneutronservices'],
+  require => File[$conf_dirs],
+}
+ini_setting { "bigswitch_consistency_interval":
+  path    => $neutron_conf_path,
+  section => 'restproxy',
+  setting => 'consistency_interval',
+  value   => '0',
+  ensure  => present,
+  notify => Exec['restartneutronservices'],
+  require => File[$conf_dirs],
+}
+
 ini_setting { "bigswitch_ssl":
   path    => $neutron_conf_path,
   section => 'restproxy',
