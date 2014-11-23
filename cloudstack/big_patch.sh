253b548c74ba9e05306c818e707fc0a8fc555e86
xinwu <xin.wu@bigswitch.com>
2014-11-23 02:11:33 -0800

    change workflow for centos

---
+++ cloudstack/big_patch.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Usage: bash big_patch.sh example.yaml
+
+# check configuration file
+if [[ -z "$*" ]]; then
+    echo -e "No configuration file is specified.\nUsage: bash big_patch.sh example.yaml"
+    exit 1
+fi
+config=$1
+if [[ ! -f ${config} ]]; then
+    echo "Configuration file does not exist"
+    exit 1
+fi
+echo "Configuration file is ${config}"
+mkdir -p /home/root/bcf
+cp ${config} /home/root/bcf/
+
+# if os is ubuntu
+python -mplatform | grep Ubuntu
+if [[ $? == 0 ]]; then
+    sudo apt-get update -y
+    sudo apt-get install -y sshpass python-yaml python-pip python-dev
+    sudo pip install futures subprocess32
+    rm /home/root/bcf/big_patch.py
+    wget --no-check-certificate https://raw.githubusercontent.com/bigswitch/deployment-support/master/cloudstack/big_patch.py -P /home/root/bcf
+    python /home/root/bcf/big_patch.py -c /home/root/bcf/${config}
+    exit 0
+fi
+
+# if os is centos
+python -mplatform | grep centos
+if [[ $? == 0 ]]; then
+    # install sshpass
+    rm /home/root/bcf/sshpass-1.05-1.el6.rf.x86_64.rpm
+    wget http://pkgs.repoforge.org/sshpass/sshpass-1.05-1.el6.rf.x86_64.rpm -P /home/root/bcf/
+    yum install -y /home/root/bcf/sshpass-1.05-1.el6.rf.x86_64.rpm
+
+    # install epel repo
+    rm /home/root/bcf/epel-release-5-4.noarch.rpm
+    wget http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm -P /home/root/bcf/
+    yum install -y /home/root/bcf/epel-release-5-4.noarch.rpm
+
+    # install python-pip and python-dev
+    rm /home/root/bcf/python-pip-0.8-1.1.noarch.rpm
+    wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/dalgaaf:/ceph:/extra/CentOS_CentOS-6/noarch/python-pip-0.8-1.1.noarch.rpm -P /home/root/bcf/
+    yum install -y /home/root/bcf/python-pip-0.8-1.1.noarch.rpm
+    yum install -y python-devel
+
+    # install subprocess32
+    pip-python install "subprocess32>=3.2.6"
+
+    rm /home/root/bcf/big_patch.py
+    wget --no-check-certificate https://raw.githubusercontent.com/bigswitch/deployment-support/master/cloudstack/big_patch.py -P /home/root/bcf/
+    python /home/root/bcf/big_patch.py -c /home/root/bcf/${config}
+    exit 0
+fi
+
+# we don't support other os
+echo "Please use centos 6.5, 6.6 or ubuntu 12.04 as cloudstack management node"
+
