3f6123d290532126eed2bbb895dc241204778867
xinwu <xin.wu@bigswitch.com>
2015-03-26 11:50:10 -0700

    fix pkg error

--- openstack/t6/bash_template/centos_7.sh
+++ openstack/t6/bash_template/centos_7.sh
@@ -8,16 +8,16 @@ fi
 
 # prepare dependencies
 rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-10.noarch.rpm
 yum update -y
 yum groupinstall -y 'Development Tools'
-yum install -y python-devel.x86_64 puppet python-pip wget libffi-devel openssl-devel
+yum install -y python-devel.x86_64 puppet python-pip wget libffi-devel.x86_64 openssl-devel
 pip install --upgrade ospurge
 pip install bsnstacklib==%(bsnstacklib_version)s
 rpm -ivh %(dst_dir)s/%(ivs_pkg)s
 if [ -f %(dst_dir)s/%(ivs_debug_pkg)s ]; then
     rpm -ivh %(dst_dir)s/%(ivs_debug_pkg)s
 fi
-rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-10.noarch.rpm
 puppet module install puppetlabs-inifile
 puppet module install jfryman-selinux
 mkdir -p /etc/puppet/modules/selinux/files
