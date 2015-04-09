eca6d73785234136dd9851bf29cc75e8871c4217
xinwu <xin.wu@bigswitch.com>
2015-04-09 14:17:56 -0700

    branch horizon for bcf3.0 beta1

--- bcf3/lib/constants.py
+++ bcf3/lib/constants.py
@@ -22,10 +22,10 @@ OS_RELEASE_TO_BSN_LIB = { 'juno' : '2015.1',
 # horizon patch
 DEPLOY_HORIZON_PATCH = True
 HORIZON_PATCH_URL = {
-    'juno' : 'https://github.com/bigswitch/horizon/archive/master.tar.gz',
+    'juno' : 'https://github.com/bigswitch/horizon/archive/juno-bcf-3.0-beta1.tar.gz',
 }
 HORIZON_PATCH_DIR = {
-    'juno' : 'horizon-master',
+    'juno' : 'horizon-juno-bcf-3.0-beta1',
 }
 HORIZON_BASE_DIR = '/usr/share/openstack-dashboard'
 
