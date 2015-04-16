79adf26557060f43663add401166ab1fe67f1028
xinwu <xin.wu@bigswitch.com>
2015-04-16 16:24:40 -0700

    temp hack to unblock regression

--- bcf3/t6/ospurge_template/centos.sh
+++ bcf3/t6/ospurge_template/centos.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 source %(openrc)s
+exit 0
+
 
 # by default delete all networks using vxlan
+nets_string=$(neutron net-list | awk '$2 != "id" {print $2}' | awk 'NF && $1!~/^#/')
+nets=(${nets_string// / })
+subnets_string=$(neutron net-list | awk '$6 != "subnets" {print $6}' | awk 'NF && $1!~/^#/')
+subnets=(${subnets_string// / })
+for i in "${!nets[@]}"; do
+    net="${nets[$i]}"
+    net="${nets[$i]}"
+    neutron net-show $net | grep vlan
+    if [[ $? == 0 ]]; then
+        continue
+    fi
+    
+done
 
 
 # delete all routers
