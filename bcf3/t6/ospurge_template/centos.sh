b29ca22e26447cf22c850ce59c5f988294f1142c
xinwu <xin.wu@bigswitch.com>
2015-04-16 17:28:22 -0700

    remove vxlan subnet only

--- bcf3/t6/ospurge_template/centos.sh
+++ bcf3/t6/ospurge_template/centos.sh
@@ -1,56 +1,36 @@
 #!/bin/bash
 
 source %(openrc)s
-exit 0
-
 
 # by default delete all networks using vxlan
 nets_string=$(neutron net-list | awk '$2 != "id" {print $2}' | awk 'NF && $1!~/^#/')
 nets=(${nets_string// / })
 subnets_string=$(neutron net-list | awk '$6 != "subnets" {print $6}' | awk 'NF && $1!~/^#/')
 subnets=(${subnets_string// / })
+declare -A subnet_to_net
 for i in "${!nets[@]}"; do
     net="${nets[$i]}"
-    net="${nets[$i]}"
+    subnet="${subnets[$i]}"
     neutron net-show $net | grep vlan
     if [[ $? == 0 ]]; then
         continue
     fi
-    
+    subnet_to_net[$subnet]=$net
 done
 
-
-# delete all routers
+# delete router interfaces using vxlan
 routers=$(neutron router-list | awk '$2 != "id" {print $2}' | awk 'NF && $1!~/^#/')
 for router in $routers; do
     # delete all subnets that have interface on router
     subnets=$(neutron router-port-list $router | awk '$0 ~ /.*subnet_id.*/ {print $0}' | awk '{print $(NF - 3)}' | tr -d ,| tr -d \")
     for subnet in $subnets; do
-        neutron router-interface-delete $router $subnet
+        if [[ ${subnet_to_net[$subnet]} != '' ]]; then
+            neutron router-interface-delete $router $subnet
+        fi
     done
-    neutron router-delete $router
 done
 
-# delete floating ips
-floatingips=$(neutron floatingip-list | awk '$2 != "id" {print $2}' | awk 'NF && $1!~/^#/')
-for floatingip in $floatingips; do
-    neutron floatingip-delete $floatingip
-done
-
-# delete neutron ports
-ports=$(neutron port-list | awk '$2 != "id" {print $2}' | awk 'NF && $1!~/^#/')
-for port in $ports; do
-    neutron port-delete $port
-done
-
-# delete all subnets
-subnets=$(neutron subnet-list | awk '$2 != "id" {print $2}' | awk 'NF && $1!~/^#/')
-for subnet in $subnets; do
+# delete all vxlan subnet
+for subnet in "${!subnet_to_net[@]}"; do
     neutron subnet-delete $subnet
-done
-
-# delete all networks
-nets=$(neutron net-list | awk '$2 != "id" {print $2}' | awk 'NF && $1!~/^#/')
-for net in $nets; do
-    neutron net-delete $net
 done
