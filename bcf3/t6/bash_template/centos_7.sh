0af82239538996f2d3939c0a4ff11e844dc4f9d0
xinwu <xin.wu@bigswitch.com>
2015-06-04 00:08:42 -0700

    ivs version bump

--- bcf3/t6/bash_template/centos_7.sh
+++ bcf3/t6/bash_template/centos_7.sh
@@ -35,7 +35,9 @@ if [[ $install_ivs == true ]]; then
         old_version=$(ivs --version | awk '{print $2}')
         old_version_numbers=(${old_version//./ })
         new_version_numbers=(${ivs_version//./ })
-        if [[ $old_version > $ivs_version ]]; then
+        if [[ ${old_version_numbers[0]} == 0 ]]; then
+            pass=true
+        elif [[ $old_version > $ivs_version ]]; then
             pass=false
         elif [[ $((${new_version_numbers[0]}-1)) -gt ${old_version_numbers[0]} ]]; then
             pass=false
