7fe5268ad1b26726109d37368a30b61d098c5af5
xinwu <xin.wu@bigswitch.com>
2015-06-11 09:01:20 -0700

    add missing pkg

--- bcf3/t6/bash_template/ubuntu_14.sh
+++ bcf3/t6/bash_template/ubuntu_14.sh
@@ -66,6 +66,8 @@ if [[ $install_ivs == true ]]; then
     if [[ $pass == true ]]; then
         dpkg --force-all -i %(dst_dir)s/%(ivs_pkg)s
         if [[ -f %(dst_dir)s/%(ivs_debug_pkg)s ]]; then
+            apt-get install -y libnl-genl-3-200
+            apt-get -f install -y
             dpkg --force-all -i %(dst_dir)s/%(ivs_debug_pkg)s
         fi
     else
